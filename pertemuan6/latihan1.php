67c8d8f49a691cac3aacfc93bfa525667cd08c78
kaila <k.zhaviaskaazzahra@gmail.com>
2026-09-25 21:33:48 +0700

    Add files via upload

---
+++ pertemuan6/latihan1.php
@@ -0,0 +1,20 @@
+<?php
+
+$a[0] = 1;
+$a[1] = 3;
+$a[2] = 5;
+
+$jumlah = count($a);
+
+echo "Jumlah array a = $jumlah <br>";
+
+$b["buah"] = "semangka";
+$b["sayur"] = "wortel";
+$b["daging"] = "ayam";
+$b["utama"] = "nasi";
+
+$jumlah = sizeof($b);
+
+echo "Jumlah array b = $jumlah <br>";
+
+?>
---
+++ pertemuan6/latihan2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Latihan Array 2</title>
+</head>
+<body>
+
+<?php
+
+$x = array("one", "two", "three");
+
+foreach ($x as $value) {
+    echo $value . "<br>";
+}
+
+?>
+
+</body>
+</html>
